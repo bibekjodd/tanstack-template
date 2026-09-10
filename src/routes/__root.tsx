a91570625f6348cfd637299f99829681bea3578b
bibekjodd <bibekjodd@gmail.com>
2026-09-10 17:42:51 +0545

    update template

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -11,7 +11,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     meta: [
       { charSet: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { title: 'TanStack Start Template' },
+      { title: 'TanStack Start' },
       {
         name: 'description',
         content: 'A TanStack Start starter with shadcn/ui, Tailwind v4 tokens, and TanStack Query.'
